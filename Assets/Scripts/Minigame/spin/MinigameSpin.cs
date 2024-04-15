e965e621c06dc260d9f8c775f8739a8c84a257be
hatdog42 <kristian.lillesand@gmail.com>
2024-04-15 14:18:43 +0200

    awrhg

---
+++ Assets/Scripts/Minigame/spin/MinigameSpin.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Minigame.spin
+{
+    public class MinigameSpin : MonoBehaviour
+    {
+        // speed er mengden grader pilen roteres per frame
+        [SerializeField]private  float speed = 3;
+        [SerializeField]private float klrange = 20;
+
+        [SerializeField]private bool irange = false;
+
+        [SerializeField]private float pilpos = 0;
+
+        [SerializeField]private float spotpos = 0;
+
+// rangev1 er fra pilposisjon - klrange til pilposisjon
+// rangev2 er fra pilposisjon til pilposisjon + klrange
+        private Vector2 rangev1 = new Vector2(0, 0);
+        private Vector2 rangev2 = new Vector2(0, 0);
+
+        [SerializeField]private RectTransform pilTransform;
+        [SerializeField]private RectTransform spotTransform;
+
+        private void Update()
+        {
+            // finne range
+            rangev1 = new Vector2(spotpos - klrange, spotpos);
+            rangev2 = new Vector2(spotpos, spotpos + klrange);
+            // sjekke om minimum er under 0 eller maximum er over 359
+            if (rangev1.x < 0)
+            {
+                rangev1 = new Vector2(rangev1.x + 360, 359);
+                rangev2 = new Vector2(0, rangev2.y);
+           
+            }
+
+            if (rangev2.y > 359)
+            {
+                rangev1 = new Vector2(rangev1.x, 359);
+                rangev2 = new Vector2(0, rangev2.y - 360);
+            }
+           
+               
+            // pil innenfor spotrange?
+            if ((pilpos >= rangev1.x && pilpos <= rangev1.y) || (pilpos >= rangev2.x && pilpos <= rangev2.y))
+            { 
+                irange = true;
+            }
+            else
+            {
+                irange = false;
+            }
+        
+            // dersom spilleren trykker, sette ny posisjon av spot
+            if (Input.GetKeyDown("space") && irange == true)
+            {
+                spotpos += Random.Range(60, 300);
+            }
+
+            if (spotpos > 360)
+            {
+                spotpos -= 360;
+                
+           
+            } 
+            spotTransform.eulerAngles = new Vector3(0, 0, spotpos);
+            if (irange == false)
+            {
+                print("miss");
+            }
+        
+            // bevege pilpos med speed, så sette spritens rotasjon til pilpos
+            pilpos += speed;
+            if (pilpos >= 360)
+            {
+                pilpos -= 360;
+            }
+            // rotere spriten
+            pilTransform.eulerAngles = new Vector3(0, 0, pilpos);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+namespace Player
+{
+    public class Movement : MonoBehaviour
+    {
+        [SerializeField] private float speed;
+        [SerializeField] private float groundDist;
+    
+        [SerializeField] private Transform InteractPoint;
+
+        [SerializeField] private LayerMask terrainLayer;
+        [SerializeField] private Rigidbody rb;
+        [SerializeField] private SpriteRenderer sr;
+
+        private void Start()
+        {
+            rb = gameObject.GetComponent<Rigidbody>();
+            InteractPoint.transform.parent.SetParent(gameObject.transform);
+        }
+
+        private void Update()
+        {
+            RaycastHit hit;
+            Vector3 castPos = transform.position;
+            castPos.y += 1;
+            if (Physics.Raycast(castPos, -transform.up, out hit, Mathf.Infinity, terrainLayer))
+            {
+                if (hit.collider != null)
+                {
+                    Vector3 movePos = transform.position;
+                    movePos.y = hit.point.y + groundDist;
+                }
+            }
+
+            float x = Input.GetAxis("Horizontal");
+            float y = Input.GetAxis("Vertical");
+            Vector3 moveDir = new Vector3(x, 0, y);
+            rb.velocity = moveDir * speed;
+
+            if (moveDir.x < 0 )
+            {
+                InteractPoint.localPosition = new Vector3(-1f, -0.5f, 0f);
+            }
+            else if (moveDir.x > 0)
+            {
+                InteractPoint.localPosition = new Vector3(1f, -0.5f, 0f);
+            }
+            else if (moveDir.z < 0)
+            {
+                InteractPoint.localPosition = new Vector3(0f, -0.5f, -1f);
+            }
+            else if (moveDir.z > 0)
+            {
+                InteractPoint.localPosition = new Vector3(0f, -0.5f, 1f);
+            }
+        
+
+
+            if (x != 0 && x < 0)
+            {
+                sr.flipX = true;
+            }
+            else if(x != 0 && x > 0)
+            {
+                sr.flipX = false;
+            }
+        }
+    }
+}
