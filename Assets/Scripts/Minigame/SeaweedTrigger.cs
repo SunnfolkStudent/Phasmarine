0edc6e90c0a8200ccb90e4e18ca80ca9f2498c95
LiveSkigelstrand <143702304+LiveSkigelstrand@users.noreply.github.com>
2024-04-22 11:48:14 +0200

    drag and drop

---
+++ Assets/Scripts/Minigame/SeaweedTrigger.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeaweedTrigger : MonoBehaviour, IInteracteble
+{
+    private MiniGameManager _miniGameManager;
+
+    private void Start()
+    {
+        _miniGameManager = GetComponent<MiniGameManager>();
+    }
+    public bool Interact(Interactor interactor)
+    {
+        _miniGameManager.CleaningMiniGameUp();
+        Destroy(gameObject);
+        return true;
+    }
+    
+    public string InteractionPrompt { get; }
+    
+}
