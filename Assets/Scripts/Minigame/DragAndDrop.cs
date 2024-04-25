5a4f28ed5f1f3af8688347717a1515de8f34eca0
LiveSkigelstrand <143702304+LiveSkigelstrand@users.noreply.github.com>
2024-04-25 12:13:04 +0200

    scac

--- Assets/Scripts/Minigame/DragAndDrop.cs
+++ Assets/Scripts/Minigame/DragAndDrop.cs
@@ -6,14 +6,19 @@ using UnityEngine;using UnityEngine.EventSystems;
 public class DragAndDrop : MonoBehaviour, IDragHandler
 {
     [SerializeField] 
-    private float dampingSpeed = 0.5f;
+    private float dampingSpeed;
     private int seaweedleft = 7;
     private RectTransform draggingObjectRectTransform;
     private Vector3 velocity = Vector3.zero;
+    private bool draggable = true;
     private MiniGameManager _miniGameManager;
+
+    [SerializeField] private Vector2 minmaxX = new Vector2(-250, 250);
+    [SerializeField] private Vector2 minmaxY = new Vector2(-250, 250);
+    
     void Start()
     {
-        _miniGameManager = GetComponent<MiniGameManager>();
+        _miniGameManager = GameObject.Find("Part").GetComponent<MiniGameManager>();
     }
 
     private void Awake()
@@ -21,6 +26,29 @@ public class DragAndDrop : MonoBehaviour, IDragHandler
         draggingObjectRectTransform = transform as RectTransform;
     }
 
+    private void Update()
+    {
+        if (draggingObjectRectTransform.position.x > minmaxX.y || 
+            draggingObjectRectTransform.position.x < minmaxX.x ||
+            draggingObjectRectTransform.position.y > minmaxY.y || 
+            draggingObjectRectTransform.position.y < minmaxY.x)
+        {
+            if (draggable) 
+                //Want to make the current object only count once
+            { seaweedleft -= 1;
+                Debug.Log(seaweedleft);
+                draggable = false;}
+            
+            
+            if (seaweedleft == 0)
+            {
+                Debug.Log("Seaweed collider");
+                _miniGameManager.CleaningMiniGameDown();
+                
+            }
+            
+        }
+    }
     public void OnDrag(PointerEventData eventData)
     {
         if (RectTransformUtility.ScreenPointToWorldPointInRectangle(draggingObjectRectTransform, eventData.position,
@@ -31,14 +59,4 @@ public class DragAndDrop : MonoBehaviour, IDragHandler
         }
     }
     
-    
-    void OnTriggerExit(Collider other)
-    {
-        seaweedleft -= 1;
-        if (seaweedleft == 0)
-        {
-            _miniGameManager.CleaningMiniGameDown();
-        }
-        //Destroy(other.gameObject);
-    }
 }
