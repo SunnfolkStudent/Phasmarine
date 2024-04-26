1b0d1620ab37e7ecdaed8c6f64f488056f145e1f
hatdog42 <kristian.lillesand@gmail.com>
2024-04-26 11:32:16 +0200

    stuff

--- Assets/Scripts/Minigame/MiniGameManager.cs
+++ Assets/Scripts/Minigame/MiniGameManager.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
@@ -24,34 +21,34 @@ public class MiniGameManager : MonoBehaviour
 
     public void CleaningMiniGameUp()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("CleaningMiniGame", LoadSceneMode.Additive);
+        SceneManager.LoadScene("CleaningMiniGame", LoadSceneMode.Additive);
         
     }
 
     public void CleaningMiniGameDown()
     {
-        UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync("CleaningMiniGame");
+        SceneManager.UnloadSceneAsync("CleaningMiniGame");
     }
     
     public void PuzzleMiniGameUp()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("PuzzleMiniGame", LoadSceneMode.Additive);
+        SceneManager.LoadScene("PuzzleMiniGame", LoadSceneMode.Additive);
         
     }
 
     public void PuzzleMiniGameDown()
     {
-        UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync("PuzzleMiniGame");
+        SceneManager.UnloadSceneAsync("PuzzleMiniGame");
     }
     
     public void SpinMiniGameUp()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("SpinMiniGame", LoadSceneMode.Additive);
+        SceneManager.LoadScene("SpinMiniGame", LoadSceneMode.Additive);
         
     }
 
     public void SpinMiniGameDown()
     {
-        UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync("SpinMiniGame");
+        SceneManager.UnloadSceneAsync("SpinMiniGame");
     }
 }
