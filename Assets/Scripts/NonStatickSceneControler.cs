3c9c0ea0f1b74c82b8d2820abaef0cf6d02633ad
hatdog42 <kristian.lillesand@gmail.com>
2024-05-02 21:46:32 +0200

    start and fade

--- Assets/Scripts/NonStatickSceneControler.cs
+++ Assets/Scripts/NonStatickSceneControler.cs
@@ -1,11 +1,34 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Playables;
 
 
 public class NonStatickSceneControler : MonoBehaviour
 {
+    [SerializeField] private PlayableDirector timline;
     public void StartGame()
     {
-        StatickSceneControler.StartGame();
+        StartCoroutine(IStartGame());
+        timline.Play();
+    }
+    public void Creedits()
+    {
+        StartCoroutine(ICreedits());
+        timline.Play();
+    }
+    public void Exit()
+    {
+        Application.Quit();
     }
     
+    
+    public IEnumerator IStartGame()
+    {
+        yield return new WaitForSeconds(1.5f);
+        StatickSceneControler.StartGame();
+    }
+    public IEnumerator ICreedits()
+    {
+        yield return new WaitForSeconds(1.5f);
+    }
 }
