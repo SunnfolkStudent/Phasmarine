67ceb4b75f8d0a2e5166a65d8503dbe3ba5ec9e3
hatdog42 <kristian.lillesand@gmail.com>
2024-05-04 16:11:13 +0200

    sfx

--- Assets/Scripts/Submarine/Refiloxigen.cs
+++ Assets/Scripts/Submarine/Refiloxigen.cs
@@ -1,15 +1,19 @@
+
 using Player;
 using UnityEngine;
-using FMODUnity;
+
 public class Refiloxigen : MonoBehaviour
 {
-    [SerializeField] private EventReference OxygenRefill;
-    
     private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             BatteryController.BatteryLevel += 5;
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        AudioManager.instance.PlayOneShot(FMODEvents.instance.oxygenRefill, transform.position);
     }
 }
